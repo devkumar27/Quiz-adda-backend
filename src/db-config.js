90e289e0c10e4c15d58bf86d4783885a7ac9e2c9
devkumar27 <dev270502@gmail.com>
2023-07-11 23:08:54 +0530

    final commit

---
+++ src/db-config.js
@@ -0,0 +1,5 @@
+// TODO: Rename the filename from TODO-db-config.js to db-config.js
+// Paste your MongoDB API key here
+module.exports = {
+	database: 'ZItFesbyGFUBNFKYhRNVVtxjoZAhStzKzT6KNRO7cCl9B5MynFgOw1IftQhCPIf1'
+}
